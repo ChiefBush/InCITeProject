1e39b34c3fd9dc1760bde60d43c6bf4293973f62
ChiefBush <80193023+ChiefBush@users.noreply.github.com>
2023-09-30 01:25:49 +0530

    Delete Main Page 2.py

--- Main Page 2.py
+++
@@ -1,5 +0,0 @@
-# Create buttons for Existing User and New User
-existing_user_btn = tk.Button(root, text="Existing User", command=on_button_click)
-new_user_btn = tk.Button(root, text="New User", command=on_button_click)
-existing_user_btn.pack()
-new_user_btn.pack()
